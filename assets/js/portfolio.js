bd3b24b37cbe98a081d06f6d77b39eab78339e39
Lokeswar <75309173+lokeshpatteda@users.noreply.github.com>
2024-10-27 19:37:11 +0530

    Add files via upload

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -291,3 +291,18 @@ $(document).ready(function($) {
     animateElements();
     $(window).scroll(animateElements);
   });
+
+
+  window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
